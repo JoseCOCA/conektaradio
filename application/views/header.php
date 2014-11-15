65b15734c15f6938f57ba643ece2e62df944bac2
Erickcoca <eaerickcoca@gmail.com>
2014-11-15 15:50:19 -0600

    menu

--- application/views/header.php
+++ application/views/header.php
@@ -6,15 +6,23 @@
 	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-	<div><img class="imagen" src="../images/head.jpg" alt="">
-	</div>
-	  <div>
+
+	   <div><img class="imagen" src="../images/head.jpg" alt="">
+	   </div>
+       <div id="contenido">
+	  <div id="menu">
 	<nav>
       <ul>
-        <li>INICIO</li>
-        <li>PROGRAMACION</li>
-        <li>MI FARMACIA</li>
-        <li>COFFEE SEED</li>
+        <li><a #href="">INICIO</a></li>
+        <li><a #href="">PROGRAMACION</a></li>
+        <li><a #href="">MI FARMACIA</a></li>
+        <li><a #href="">COFFEE SEED</a></li>
+        <li><a #href="">ANTROS REST & BAR</a></li>
+        <li><a #href="">NOTICIAS</a></li>
+        <li><a #href="">PODCAST</a></li>
+        <li><a #href="">MUSICA</a></li>
+        <li><a #href="">CONTAKTANOS</a></li>
       </ul>
   </nav>
+  </div>
     </div>
