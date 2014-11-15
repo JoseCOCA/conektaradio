538104c5df6fe098ae3a36a5df1f5c7d7323b1fc
Erickcoca <eaerickcoca@gmail.com>
2014-11-15 14:49:52 -0600

    DESMADRE

--- application/views/home.php
+++ application/views/home.php
@@ -2,26 +2,19 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
-	<style type="text/css">
-body
-{
-	width: 100%;
-	margin-right: 0px;
-	margin-left: 0px;
-	margin-bottom: 0px;
-	margin-top: 0px;
-    
-}
-.imagen
-{
-	width: 100%;
-	height: 300px;
-}
-	</style>
+	<title>Home</title>
+	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 	<div><img class="imagen" src="../images/head.jpg" alt="">
 	</div>
+	  <div>
+      <ul>
+        <li><a> INICIO</a></li>
+        <li><a>PROGRAMACION</a></li>
+        <li><a>MI FARMACIA</a></li>
+        <li><a>COFFEE SEED</a></li>
+      </ul>
+    </div>
 </body>
 </html>
