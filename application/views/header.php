54911b3f0b763f6023417d8eee6f0da54e3fdab8
Jose Coca <josecoca0890@gmail.com>
2015-01-22 09:06:11 -0600

    maquetacion de nuevo sitio

--- application/views/header.php
+++ application/views/header.php
@@ -1,28 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Home</title>
-	<link rel="stylesheet" href="../css/style.css">
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/slides.css">
+    <script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
+    <script type="text/javascript" src="js/jquery.pikachoose.min.js"></script>
+    <script type="text/javascript" src="js/jquery.touchwipe.min.js"></script>
+    
+    <script language="javascript">
+        $(document).ready(
+            function (){
+            $("#pikame").PikaChoose({carousel:true});
+        });
+    </script>
 </head>
 <body>
 
-	   <div><img class="imagen" src="../images/head.jpg" alt="">
-	   </div>
-       <div id="contenido">
-	  <div id="menu">
-	<nav>
-      <ul>
-        <li><a #href="">INICIO</a></li>
-        <li><a #href="">PROGRAMACION</a></li>
-        <li><a #href="">MI FARMACIA</a></li>
-        <li><a #href="">COFFEE SEED</a></li>
-        <li><a #href="">ANTROS REST & BAR</a></li>
-        <li><a #href="">NOTICIAS</a></li>
-        <li><a #href="">PODCAST</a></li>
-        <li><a #href="">MUSICA</a></li>
-        <li><a #href="">CONTAKTANOS</a></li>
-      </ul>
-  </nav>
-  </div>
+    <div><img class="headerImg" src="images/head.jpg" alt="">
+    </div>
+    <div id="menu">
+        <nav>
+            <ul>
+                <li><a href="">INICIO</a></li>
+                <li><a href="">PROGRAMACION</a></li>
+                <li><a href="">MI FARMACIA</a></li>
+                <li><a href="">COFFEE SEED</a></li>
+                <li><a href="">ANTROS REST & BAR</a></li>
+                <li><a href="">NOTICIAS</a></li>
+                <li><a href="">PODCAST</a></li>
+                <li><a href="">MUSICA</a></li>
+                <li><a href="">CONTAKTANOS</a></li>
+            </ul>
+        </nav>
     </div>
---
+++ application/views/home.php
@@ -0,0 +1,55 @@
+
+<div id="contenido">
+    <section class="Principal">
+	    <div class="bPat">
+	    	<div id="topBan"><img src="images/banners/MiFarmacia.png" alt=""></div>
+	    </div>
+	    <div class="prog">
+	    	<div id="headProg"><img src="images/progsHead.png" alt=""></div>
+	    	<div id="prog-cont">
+	    		<div class="col-3"><img src="images/prog1.png" alt=""></div>
+	    		<div class="col-3"><img src="images/prog2.png" alt=""></div>
+	    		<div class="col-3"><img src="images/prog3.png" alt=""></div>
+	    		<div class="col-3"><img src="images/prog4.png" alt=""></div>
+	    		<div class="col-3"><img src="images/prog5.png" alt=""></div>
+	    		<div class="col-3"><img src="images/prog6.png" alt=""></div>
+	    		<div class="col-3"><img src="images/prog7.png" alt=""></div>
+	    		<div class="col-3"><img src="images/prog8.png" alt=""></div>
+	    		<div class="col-3"><img src="images/prog9.png" alt=""></div>
+	    	</div>
+	    </div>
+	    <div class="noticias">
+	    	<img src="images/noticias1.png" alt="">
+	    	<div>
+	    		<ul id="pikame" >
+					<li><a href="#"><img src="images/1.jpg"/></a><span>This is an example of the basic theme.</span></li>
+					<li><a href="#"><img src="images/2.jpg"/></a><span>jCarousel is supported and can be integrated with PikaChoose!</span></li>
+					<li><a href="#"><img src="images/3.jpg"/></a><span>Be sure to check out <a href="#">PikaChoose.com</a> for updates.</span></li>
+				</ul>
+	    	</div>
+	    	
+	    </div>
+	</section>
+    <section class="anuncios">
+    	<div class="redes">
+    		<img src="images/twitter.png" alt="">
+    		<img src="images/facebook.png" alt="">
+    		<img src="images/youTube.png" alt="">
+    	</div>
+    	<div id="chat">
+    		<img src="images/chat.png" alt="">
+    	</div>
+    	<div id="comenta">
+    		<a href=""><img src="images/comentarios.png" alt=""></a>
+    	</div>
+    	<div id="anunciate"> 
+    		<a href=""><img src="images/anunciate.png" alt=""></a>
+    	</div>
+    	<div id="eventos">
+    		<a href=""><img src="images/eventos.png" alt=""></a>
+    	</div>
+    </section>
+    <section class="slogan">
+    	<img src="images/slogan.png" alt="">
+    </section>
+</div>
